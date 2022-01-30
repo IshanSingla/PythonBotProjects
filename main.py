3c92ea3e8dfae1398f1eca2af2e77e638d1c57de
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-01-30 07:29:59 -0800

    done

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from datetime import datetime
 import json, firebase_admin, sys,telethon,asyncio,requests
 from tokenize import Token
 from telethon import *
+import logging
+import os
+from asyncio import sleep
 
 from telethon import functions, types
 from telethon import TelegramClient, events, Button
@@ -9,6 +12,21 @@ from telethon.sessions import StringSession
 from telethon.errors.rpcerrorlist import ChatAdminRequiredError
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins, ChatAdminRights
 from telethon.tl.functions.channels import EditBannedRequest
+from decouple import config
+
+import logging
+import os
+from asyncio import sleep
+
+from decouple import config
+
+from telethon import TelegramClient, events, Button
+from telethon.sessions import StringSession
+from telethon.errors.rpcerrorlist import ChatAdminRequiredError
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights
+
+
 
 from telethon.errors.rpcerrorlist import ChatAdminRequiredError
 from firebase_admin import credentials,db
@@ -20,11 +38,29 @@ APP_ID = 12468937
 API_HASH = "84355e09d8775921504c93016e1e9438"
 BOT_TOKEN = "5170782972:AAFba1KKvu7DzcX_4utjQqzRVidmurFMCbE"
 OWNERS=[1854668908, 1303790979, 1322941082, 5217968098]
-client = telethon.TelegramClient("cli", api_id=APP_ID , api_hash=API_HASH).start(bot_token=BOT_TOKEN)
+SESSION = config("SESSION")
+#client = telethon.TelegramClient("cli", api_id=APP_ID , api_hash=API_HASH).start(bot_token=BOT_TOKEN)
+
+try:
+    client = TelegramClient(
+        StringSession(SESSION), api_id=API_ID, api_hash=API_HASH).start()
+
+except Exception as e:
+    log.warning(e)
+    exit(1)
+    
+    
+try:
+    bot = TelegramClient(None, api_id=API_ID, api_hash=API_HASH).start(bot_token=BOT_TOKEN)
+
+except Exception as e:
+    logging.warning(e)
+    exit(1)   
+    
 
 
 
-@client.on(events.NewMessage(incoming=True, pattern=r"\.adding"))
+@bot.on(events.NewMessage(incoming=True, pattern=r"\.adding"))
 async def _(e):
     if e.sender_id in OWNERS:
         TOK=(db.reference(f"/Members/Tok/")).get()
@@ -47,7 +83,7 @@ async def _(e):
     else:
         await e.reply("You can not use me\nContact: @IshanSingle_xD\n\nMade with ❤️ By @InducedBots")
 
-@client.on(events.NewMessage(incoming=True, pattern=r"\.ping"))
+@bot.on(events.NewMessage(incoming=True, pattern=r"\.ping"))
 async def ping(e):
     if e.sender_id in OWNERS:
         start = datetime.now()
@@ -59,13 +95,13 @@ async def ping(e):
     else:
         await e.reply("You can not use me\nContact: @IshanSingle_xD\n\nMade with ❤️ By @InducedBots")
 
-@client.on(events.NewMessage(incoming=True, pattern=r"\.restart"))    
+@bot.on(events.NewMessage(incoming=True, pattern=r"\.restart"))    
 async def restart(e):
     await e.reply("**Bot Is Restarting...\n\nMade with ❤️ By @InducedBots**")
     os.execl(sys.executable, sys.executable, "-m", "main")
 
     
-    
+#approve admin from here     
     
 async def get_waiting(chat_id):
     try:
@@ -91,10 +127,81 @@ async def get_waiting(chat_id):
     userids = [i.user_id for i in users.importers]
     return "**Total no of user who requesting to join**: {}\n\n".format(users.count), userids
 
-@client.on(events.NewMessage(incoming=True,pattern=r"\.approveall",))
+
+@bot.on(events.NewMessage(incoming=True, pattern="^/waiting", from_users=OWNERS))
+async def reply_waits(event):
+    mid = chat = None
+    if event.is_private:
+        try:
+            mid = event.text.split(" ")[1]
+        except IndexError:
+            await event.reply(
+                "Please provide a chat ID or username, or use this command in that chat/channel."
+            )
+            return
+    else:
+        mid = event.chat_id
+    try:
+        mid = int(mid)
+        chat = (await client.get_entity(mid)).id
+    except ValueError:
+        chat = (await client.get_entity(mid)).id
+    msg, users = await get_waiting(chat)
+    await event.reply(msg)
+
+
+@bot.on(events.NewMessage(incoming=True,from_users=OWNERS,pattern="^/approveall",))
 async def approvealll(event):
     mid = chat = None
-    await event.reply("Start")
+    xx = await event.reply("Hold on.")
+    if event.is_private:
+        try:
+            mid = event.text.split(" ")[1]
+        except IndexError:
+            await xx.edit(
+                "Please provide a chat ID or username, or use this command in that chat/channel."
+            )
+            return
+    else:
+        mid = event.chat_id
+    try:
+        mid = int(mid)
+        chat = (await client.get_entity(mid)).id
+    except ValueError:
+        chat = (await client.get_entity(mid)).id
+    msg, users = await get_waiting(chat)
+    if msg.startswith("Please") or msg.startswith("No"):
+        await xx.edit(msg)
+        return
+    else:
+        dn = fail = 0
+        err = None
+        while len(users) > 0:
+            for i in users:
+                try:
+                    await client(
+                        functions.messages.HideChatJoinRequestRequest(
+                            chat, user_id=int(i), approved=True
+                        )
+                    )
+                    dn += 1
+                except Exception as e:
+                    fail += 1
+                    err = e
+            msg, users = await get_waiting(chat)
+            if msg.startswith("Please") or msg.startswith("No"):
+                await xx.edit(msg)
+                break
+        msg = "__Approved {} user(s).__".format(dn)
+        if fail != 0:
+            msg += "\n__Failed to approve {} user(s).__".format(fail)
+            msg += "\n\n**Logs Forward this to @Vexana_Support**: {}".format(err)
+    await xx.edit(msg)
+
+
+@bot.on(events.NewMessage(incoming=True,from_users=OWNERS,pattern="^/disapproveall",))
+async def approvealll(event):
+    mid = chat = None
     if event.is_private:
         try:
             mid = event.text.split(" ")[1]
@@ -128,11 +235,12 @@ async def approvealll(event):
             except Exception as e:
                 fail += 1
                 err = e
-        msg = "Disapproved {} user(s).".format(dn)
+        msg = "__Disapproved {} user(s).__".format(dn)
         if fail != 0:
-            msg += "\nFailed to disapprove {} user(s).".format(fail)
-            msg += "\n\nLogs Forward this to @Vexana_Support: {}".format(err)
+            msg += "\n__Failed to disapprove {} user(s).__".format(fail)
+            msg += "\n\n**Logs Forward this to @Vexana_Support**: {}".format(err)
     await event.reply(msg)
+
 
   
 
@@ -153,5 +261,5 @@ if len(sys.argv) not in (1, 3, 4):
 else:
     try:
         client.run_until_disconnected()
-    except Exception as e:
+        except Exception as e:
         pass
